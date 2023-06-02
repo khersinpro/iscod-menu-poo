c3b73ac6d52953be09e77dd38ae436b51e677f58
kevin <khersinpro@gmail.com>
2023-06-02 14:45:29 +0200

    Ajout des fonction de génération/modification du menu, ajout du style

--- javascript/Models/Menu.js
+++ javascript/Models/Menu.js
@@ -1,36 +1,132 @@
 class Menu 
 {
-    // •	labels est un tableau de string (les items du menu).
+    // labels est un tableau de string (les items du menu).
     #labels;
-    // •	esp est l’espacement en pixels entre deux items (devrait être pair).
+    
+    // esp est l’espacement en pixels entre deux items (devrait être pair).
     #esp;
-    // •	disp définit l’orientation du menu (0 = horizontal, 1 = vertical).
+
+    // disp définit l’orientation du menu (0 = horizontal, 1 = vertical).
     #disp;
 
+    // conteneur des li
+    #htmlUlElement;
+
+    /**
+     * @param {string[]}    labels  Le nom des labels a ajouter au menu
+     * @param {int}         esp     Espacement entre les label  
+     * @param {smallint}    disp    Disposition du menu (0 = horizontal, 1 = vertical)
+     */
     constructor(labels = [], esp = 20, disp = 0)
     {
-        this.#labels    =   labels;
-        this.#esp       =   esp;
-        this.#disp      =   disp;
+        this.#labels          =   labels;
+        this.#esp             =   esp;
+        this.#disp            =   disp;
+        this.#htmlUlElement   =   document.createElement('ul');
+    }
+
+    /**
+     * Modifie l'espacement entre les items du menu
+     * @param {int} esp 
+     */
+    setEsp(esp)
+    {
+        this.#esp = esp;
+        this.applyStyleForAllLi();
+        return this;
+    }
+
+    /**
+     * Modifie l'orientation du menu
+     * @param {int} disp 
+     */
+    setDisp(disp)
+    {
+        this.#disp = disp;
+        this.ApplyStyleForUl();
+        this.applyStyleForAllLi();
+        return this;
+    }
+
+    /**
+     * Ajoute un item au menu
+     * @param {string} label 
+     */
+    addItem(label)
+    {
+        this.#labels.push(label);
+        this.addItemListForUl(label);
+        this.applyStyleForAllLi();
+        return this;
     }
 
     /**
      * Fonction qui génére les labels (les items du menu HTMLIElement) 
      */
+    generatieListItem(label)
+    {
+        const textNode      =   document.createTextNode(label);     
+        const li            =   document.createElement('li');
+        li.appendChild(textNode)
+        return li;
+    }
 
     /**
-     * Fonction qui intégre les labels dans le conteneur du menu
+     * Fonction qui retourne l'objet menu
      */
-
-     /**
-      * Fonction qui génére le conteneur du menu (HTMLListElement)
-      */
-
-     /**
-      * Fonction qui retourne l'objet menu
-      */
     getObjDOM() 
     {
-    
+        this.#labels.forEach(label => this.addItemListForUl(label));
+        this.ApplyStyleForUl();
+        this.applyStyleForAllLi();
+
+        return this.#htmlUlElement;
+    }   
+
+
+    /**
+     * Génére le style pour un élement li
+     * @returns string
+     */
+    generateLiStyle()
+    {
+        const liHoriztonTalStyle    =   `margin-right: ${this.#esp}px; margin-left: ${this.#esp}px; margin-top: 30px; margin-bottom: 30px;`;
+        const liVerticalStyle       =   `margin-right: 30px; margin-left: 30px; margin-top: ${this.#esp}px; margin-bottom: ${this.#esp}px;`;
+        return this.#disp === 0 ? liHoriztonTalStyle : liVerticalStyle;
+    }
+
+    /**
+     * Génére le style pour un élément ul
+     * @returns string
+     */
+    generateUlStyle()
+    {
+        const ulBaseStyle         =   "display: flex; list-style: none; background: #006266; color: #ffffff; font-weight: bold; text-align: center; height: 100%; width: 100%;";
+        const ulHorizontalStyle   =   ulBaseStyle + "flex-wrap: wrap; justify-content: center;";
+        const ulVerticalStyle     =   ulBaseStyle + "flex-direction: column;";
+        return this.#disp === 0 ? ulHorizontalStyle : ulVerticalStyle;
+    }
+
+    /**
+     * Applique le style css correspondant pour chaque li de l'élement ul
+     */
+    applyStyleForAllLi()
+    {
+        const listLi = this.#htmlUlElement.querySelectorAll('li');
+        const style  = this.generateLiStyle();
+        listLi.forEach(li => li.style.cssText = style)
+    }
+
+    /**
+     * Applique le style pour un élément ul
+     */
+    ApplyStyleForUl()
+    {
+        this.#htmlUlElement.style.cssText = this.generateUlStyle();
+    }
+
+    addItemListForUl(label)
+    {
+        this.#htmlUlElement.appendChild(this.generatieListItem(label));
     }
 }
--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,5 @@
-let labels = ["Accueil", "Produits", "Contact"]; 
+let labels = ["Accueil", "Produits","Contact"]; 
 let menu = new Menu(labels);
-let menuObj = menu.getObjDOM();
-
-menuObj.addEventListener('menu_click', evt => console.log(`Clic sur ${labels[evt.detail.index]}`)); 
-
-out.appendChild(menuObj);
+let menuObj = menu.getObjDOM(); menuObj.addEventListener('menu_click', evt =>
+console.log(`Clic sur ${labels[evt.detail.index]}`)); out.appendChild(menuObj);
+setTimeout(() => menu.setEsp(100).setDisp(1).addItem('Test'), 5000);
